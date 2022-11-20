dadbd273605cb9b694e076631203e7c482571462
Junaid <mohammad.junaidarif@mail.mcgill.ca>
2022-11-20 04:42:46 -0500

    website
    Initial Website Commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/launch')
+def launch():
+    celsius = request.args.get("celsius", "")
+    return fahrenheit_from()
+    # return render_template("launch.html", celsius="celsius")
+
+
+@app.route('/launch', methods=['POST', 'GET'])
+def fahrenheit_from():
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(42) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+
+# @app.route("/run", methods=['POST', "GET"])
+# def run():
+#     output = request.form.to_dict()
+#     launch = output["launch"]
+#
+#     return render_template("index.html", launch="launch")
+
+
+# @app.route("/launch.html")
+# def blast():
+#     render_template("launch.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
